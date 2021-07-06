a38aa128011e058aca512d7303d8eb113e070531
Yolanda Su <yolanda.su@sequoiadb.com>
2021-07-06 16:23:45 +0000

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -37,36 +37,4 @@ tieredstore:
 EOF
 
 helm repo add alluxio-charts https://alluxio-charts.storage.googleapis.com/openSource/2.6.0
-#helm install alluxio -f alluxio.yaml alluxio-charts/alluxio --wait
-
-# The metadata microservice must be built before installing
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.planetrover.io/sequoiadp/parquet_metadata_microservice_golang_thrift.git
-cd parquet_metadata_microservice_golang_thrift/src
-thrift -r --gen go metadata.thrift
- cd gen-go
-    # Initialize generated go modules
-for i in parquet_metadata metadata
-do
-    cd $i
-    go mod init $i
-    go mod tidy
-    sed -i "s/github.com\/apache\/thrift\/lib\/go\/thrift v0.0.0-20210120171102-e27e82c46ba4/github.com\/apache\/thrift v0.13.0/g" go.mod
-    cd ..
-done
-# Insert reference to image pull secret into deployment yaml
-cd ../..
-sed -i "s@# <CI image secret is inserted here by pipeline in the helm build job>@imagePullSecrets:\\n      - name: login-cred@g" helmchart/templates/my-image.yaml
-# Replace image with gitlab registry image url
-sed -i "s@metadata_microserivce_demo:latest@gitlab.planetrover.io:5050/sequoiadp/parquet_metadata_microservice_golang_thrift:latest@g" helmchart/templates/my-image.yaml
-# Generate imae pull secret using gitlab registry credentials
-kubectl create secret docker-registry login-cred --namespace=${KUBE_NAMESPACE} --docker-server=${CI_REGISTRY} --docker-username=${CI_REGISTRY_USER} --docker-password=${CI_REGISTRY_PASSWORD}
-helm upgrade --install metadata-microservice ./helmchart
-    
-# run metastore microservice
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.planetrover.io/sequoiadp/hive-metastore-helmchart.git
-cd hive-metastore-helmchart
-# replace image
-sed -i "s@{{ .Values.image.registry}}/{{ .Values.image.repository }}:{{ .Values.image.tag }}@gitlab.planetrover.io:5050/sequoiadp/hive-metastore-helmchart:latest@g" templates/statefulset.yaml
-# add secret
-sed -i "s@{{- end }}@{{- end }}\\n      imagePullSecrets:\\n      - name: login-cred@g" templates/statefulset.yaml
-helm upgrade --install metastore-microservice .
+helm install alluxio -f alluxio.yaml alluxio-charts/alluxio --wait
