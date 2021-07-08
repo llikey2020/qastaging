c912482cedee691c751eb12ce69f81e88086c620
Matt Van Dijk <matthew@sequoiadb.com>
2021-07-08 15:12:56 +0000

    Update setup-env.sh, .gitlab-ci.yml, cleanup-env.sh files

--- setup-env.sh
+++ setup-env.sh
@@ -38,3 +38,40 @@ EOF
 
 helm repo add alluxio-charts https://alluxio-charts.storage.googleapis.com/openSource/2.6.0
 helm install alluxio -f alluxio.yaml alluxio-charts/alluxio --wait
+
+cat << EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: ${MYSQL_SVC_NAME}
+spec:
+  ports:
+  - port: 3306
+  selector:
+    app: ${MYSQL_SVC_NAME}
+  clusterIP: None
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${MYSQL_SVC_NAME}
+spec:
+  selector:
+    matchLabels:
+      app: ${MYSQL_SVC_NAME}
+  template:
+    metadata:
+      labels:
+        app: ${MYSQL_SVC_NAME}
+    spec:
+      containers:
+      - image: mysql:${MYSQL_VERSION}
+        name: ${MYSQL_SVC_NAME}
+        env:
+          # Use secret in real usage
+        - name: MYSQL_ROOT_PASSWORD
+          value: ${MYSQL_ROOT_PASSWORD}
+        ports:
+        - containerPort: 3306
+          name: ${MYSQL_SVC_NAME}
+EOF
