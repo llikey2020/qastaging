5be643f7dcc4875e38b8186bc1a799badd310d3f
Matt Van Dijk <matthew@sequoiadb.com>
2021-07-08 17:26:22 +0000

    Update cleanup-env.sh

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -6,4 +6,4 @@ kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_WA
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_DEPENDENCY_DIR} || true
 helm uninstall alluxio alluxio-charts/alluxio || true
 kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
-kubectl delete service/${MYSQL_SVC_NAME},deployment/${MYSQL_SVC_NAME}
+kubectl delete service/${MYSQL_SVC_NAME} deployment/${MYSQL_SVC_NAME}
