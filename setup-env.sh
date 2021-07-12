debcfeeb8f0f8f0097d943188977c7fb4425ca5c
Matt Van Dijk <matthew@sequoiadb.com>
2021-07-12 18:09:25 +0000

    Add hive-metastore to setup

--- setup-env.sh
+++ setup-env.sh
@@ -75,3 +75,16 @@ spec:
         - containerPort: 3306
           name: ${MYSQL_SVC_NAME}
 EOF
+
+DOCKER_LOGIN_SECRET=login-cred
+kubectl create secret docker-registry ${DOCKER_LOGIN_SECRET} --namespace=${KUBE_NAMESPACE} --docker-server=${CI_REGISTRY} --docker-username=${CI_REGISTRY_USER} --docker-password=${CI_REGISTRY_PASSWORD}
+
+wget --header="JOB-TOKEN: ${CI_JOB_TOKEN}" ${CI_API_V4_URL}/projects/44/packages/generic/helm-chart/0.1.0/hive-metastore-0.1.0.tgz
+tar xf hive-metastore-0.1.0.tgz
+
+helm upgrade --install \
+  --set image.pullSecrets=${DOCKER_LOGIN_SECRET} \
+  --set image.registry=${CI_REGISTRY} \
+  --set image.repository=sequoiadp/hive-metastore-helmchart \
+  --set image.tag=latest \
+  hive-metastore-service hive-metastore
