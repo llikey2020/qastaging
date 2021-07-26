4baf6e9c69652f94d8c90809a5ff651a2ec8b998
Yolanda Su <yolanda.su@sequoiadb.com>
2021-07-26 14:52:08 +0000

    Update setup-env.sh

--- services/history/setup-env.sh
+++ services/history/setup-env.sh
@@ -6,9 +6,6 @@ wget --header="JOB-TOKEN: ${CI_JOB_TOKEN}" ${CI_API_V4_URL}/projects/55/packages
 tar -zxf history-server-0.1.0.tgz
 helm install history-server --set eventLog.alluxioService=${ALLUXIO_SVC} --set eventLog.dir=${SPARK_EVENTLOG_DIR} history-server/ --wait
 
-while [ $(kubectl get pod -l app.kubernetes.io/name=history-server -o jsonpath="{.items[0].status.phase}") != 'Running'  ]; do
+while [ $(kubectl get pod -l app=history-server -o jsonpath="{.items[0].status.phase}") != 'Running'  ]; do
   sleep 1
 done
-
-kubectl describe pods
-kubectl get pods
