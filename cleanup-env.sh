3209b89d1b5ec0702cded4613175eeb13249bbf1
Yolanda Su <yolanda.su@sequoiadb.com>
2021-07-27 16:01:02 -0400

    Add rule for cleanup job

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -6,10 +6,8 @@ kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_WA
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_DEPENDENCY_DIR} || true
 helm uninstall alluxio alluxio-charts/alluxio || true
 kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
-kubectl delete service/${MYSQL_SVC_NAME} deployment/${MYSQL_SVC_NAME}
+kubectl delete service/${MYSQL_SVC_NAME} deployment/${MYSQL_SVC_NAME} || true
 helm uninstall zeppelin-service || true
 helm uninstall hive-metastore-service || true
 helm uninstall metadata-service || true
-kubectl delete secret login-cred
-
 helm uninstall history-server || true
