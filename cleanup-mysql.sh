cb690d7bd572be1ae0309b19b8a9a356caefcd3b
Matt Van Dijk <matthew@sequoiadb.com>
2021-08-11 19:59:47 +0000

    Update staging.yml, .gitlab-ci.yml, services/mysql.yml, spark.yml, services/frontend.yml, services.yml, services/alluxio.yml, services/history.yml, services/schema.yml, services/metadata.yml, services/zeppelin.yml files Deleted services/template.yml, cleanup-env.sh, cleanup-mysql.sh, services/history/cleanup-env.sh, services/frontend/pipeline.yml, services/zeppelin/setup-env.sh, services/alluxio/setup-env.sh, services/alluxio/cleanup-env.sh, services/alluxio/pipeline.yml, services/frontend/cleanup-env.sh, services/frontend/setup-env.sh, services/zeppelin/cleanup-env.sh, services/zeppelin/pipeline.yml, services/history/pipeline.yml, services/history/setup-env.sh, services/hive/setup-env.sh, services/hive/cleanup-env.sh, services/hive/pipeline.yml, services/metadata/cleanup-env.sh, services/metadata/pipeline.yml, services/metadata/setup-env.sh, setup-mysql.sh files

--- cleanup-mysql.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env sh
-
-set -ex
-
-kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
-kubectl delete service/${MYSQL_SVC_NAME} deployment/${MYSQL_SVC_NAME} || true]
